15cb28715b4856487b20c550b96ad14319de53ad
Busfreak <martin@middeke.de>
2016-03-11 12:56:17 +0100

    Changes in Text Helper
    $this->e()
    $this->text->e()

--- Template/app/color_filter.php
+++ Template/app/color_filter.php
@@ -3,7 +3,7 @@
         <ul class="color_filter">
           <li class="color_filter"><div class="color-color_filter"><a href="#" class="filter-helper" data-filter="status:open"><?= t('Reset filters') ?></a></div></li>
         <?php foreach ($this->task->colors->getColors(isset($project)?$project['id']:0) as $color_id => $color_name): ?>
-          <li class="color_filter"><div class="color-color_filter color-<?= $color_id ?>"><a href="#" class="filter-helper" data-filter='color:"<?= $this->e($color_id) ?>"'><?= $this->e($color_name) ?></a>&nbsp;|&nbsp;<a href="#" class="filter-helper" data-append-filter='color:"<?= $this->e($color_id) ?>"'><?= t('append') ?></a></div></li>
+          <li class="color_filter"><div class="color-color_filter color-<?= $color_id ?>"><a href="#" class="filter-helper" data-filter='color:"<?= $this->text->e($color_id) ?>"'><?= $this->text->e($color_name) ?></a>&nbsp;|&nbsp;<a href="#" class="filter-helper" data-append-filter='color:"<?= $this->text->e($color_id) ?>"'><?= t('append') ?></a></div></li>
         <?php endforeach ?>
         </ul>
       </div>
