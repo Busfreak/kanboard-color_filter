9b67dea1aefa5afd0e35aee7f857ab67012804de
Busfreak <martin@middeke.de>
2016-03-11 16:54:35 +0100

    dropdown changed to current icon style

--- Template/app/color_filter.php
+++ Template/app/color_filter.php
@@ -1,9 +1,9 @@
-      <div class="dropdown filters">
-        <i class="fa fa-caret-down"></i> <a href="#" class="dropdown-menu"><?= t('Color coding') ?></a>
-        <ul class="color_filter">
-          <li class="color_filter"><div class="color-color_filter"><a href="#" class="filter-helper" data-filter="status:open"><?= t('Reset filters') ?></a></div></li>
+<div class="dropdown">
+    <a href="#" class="dropdown-menu dropdown-menu-link-icon" title="<?= t('Color coding') ?>"><i class="fa fa-paint-brush fa-fw"></i><i class="fa fa-caret-down"></i></a>
+    <ul>
+        <li><a href="#" class="filter-helper filter-reset" data-filter="status:open"><?= t('Reset filters') ?></a></li>
         <?php foreach ($this->task->colors->getColors(isset($project)?$project['id']:0) as $color_id => $color_name): ?>
-          <li class="color_filter"><div class="color-color_filter color-<?= $color_id ?>"><a href="#" class="filter-helper" data-filter='color:"<?= $this->text->e($color_id) ?>"'><?= $this->text->e($color_name) ?></a>&nbsp;|&nbsp;<a href="#" class="filter-helper" data-append-filter='color:"<?= $this->text->e($color_id) ?>"'><?= t('append') ?></a></div></li>
+        <li class="color_filter"><div class="color-color_filter color-<?= $color_id ?>"><a href="#" class="filter-helper" data-filter='color:"<?= $this->text->e($color_id) ?>"'><?= $this->text->e($color_name) ?></a>&nbsp;|&nbsp;<a href="#" class="filter-helper" data-append-filter='color:"<?= $this->text->e($color_id) ?>"'><?= t('append') ?></a></div></li>
         <?php endforeach ?>
-        </ul>
-      </div>
+    </ul>
+</div>
