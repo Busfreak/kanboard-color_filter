bf11c45ce1328036a470840c2649c5758eebd678
Busfreak <martin@middeke.de>
2017-03-21 15:12:51 +0100

    improve filter div for KB 1.0.40

--- Template/app/color_filter.php
+++ Template/app/color_filter.php
@@ -3,7 +3,7 @@
     <ul>
         <li><a href="#" class="filter-helper filter-reset" data-filter="status:open"><?= t('Reset filters') ?></a></li>
         <?php foreach ($this->ColorsHelper->getColors(isset($project)?$project['id']:0) as $color_id => $color_name): ?>
-        <li class="color_filter"><div class="color-color_filter color-<?= $color_id ?>"><a href="#" class="filter-helper" data-filter='color:"<?= $this->text->e($color_id) ?>"'><?= $this->text->e($color_name) ?></a>&nbsp;|&nbsp;<a href="#" class="filter-helper" data-append-filter='color:"<?= $this->text->e($color_id) ?>"'><?= t('append') ?></a></div></li>
+        <li class="color_filter"><div class="color-picker-square color-<?= $color_id ?>"></div><div class="color-picker-label"><a href="#" class="filter-helper" data-filter='color:"<?= $this->text->e($color_id) ?>"'><?= $this->text->e($color_name) ?></a>&nbsp;|&nbsp;<a href="#" class="filter-helper" data-append-filter='color:"<?= $this->text->e($color_id) ?>"'><?= t('append') ?></a></div></li>
         <?php endforeach ?>
     </ul>
 </div>
